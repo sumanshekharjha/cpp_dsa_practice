ff1da7f5e78b2793c3f8c7892ec1ce6717d7da8d
sumanshekharjha <shekharsumanmac4@gmail.com>
2026-06-07 18:05:38 +0530

    2d array

---
+++ 4.array_vector/2darray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+//2d arrays are also array of arrays
+int main(){
+    int arr[5][8]={{1,2,3},{4,5,6},{23,54,12}};
+    cout<<arr[0][2];
+    
+
+} 
