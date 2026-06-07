040dfa95d47c84132acdeb563a51b96d8f4aa3d3
sumanshekharjha <shekharsumanmac4@gmail.com>
2026-06-07 18:14:36 +0530

    array

--- 4.array_vector/arr_vector.cpp
+++ 4.array_vector/arr_vector.cpp
@@ -224,3 +224,14 @@ using namespace std;
 //         return nums;
 //     }
 
+//8. leetcode 217
+      // bool containsDuplicate(vector<int>& nums) {
+      //       sort(nums.begin(),nums.end());
+      //       for(int i=0;i<nums.size()-1;i++){
+      //             int j=i+1;
+      //             if(nums[i]==nums[j]){
+      //             return true;
+      //             }
+      //       }
+      //       return false;
+      // }
