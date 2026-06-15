76cf0d8646a34a4683ff5d53dbb46a5eb6d609ba
sumanshekharjha <shekharsumanmac4@gmail.com>
2026-06-15 11:09:22 +0530

    pascaltriangle

---
+++ 4.array_vector/2darray/pascaltri.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main(){
+    int m,n;
+    cout<<"enter the no. of rows: ";
+    cin>>m;
+    vector< vector<int> > v;
+      for(int i=1; i<=m;i++) {
+      vector<int> a (i);
+       v. push_back(a) ;
+}  
+   
+    for(int i=0;i<m;i++){
+        for(int j=0;j<=i;j++){
+            if(j==i||j==0) v[i][j]=1;
+            else{
+                v[i][j] = v[i-1][j]+v[i-1][j-1];
+            }
+        }
+    }
+    for(int i = 0; i < m; i++) { 
+    for(int j = 0; j <= i; j++) {
+        cout << v[i][j] << " ";
+    }
+    cout << endl;
+}
+    
+    
+
+}
