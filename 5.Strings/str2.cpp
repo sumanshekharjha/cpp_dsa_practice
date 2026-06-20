02f1dd16320e5e3a4ffd6f323fd3ea40d87d0bfa
sumanshekharjha <shekharsumanmac4@gmail.com>
2026-06-20 16:37:15 +0530

    strings

---
+++ 5.Strings/str2.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+{
+    string str = "shekhar";
+    cout<<str;
+}
+cout<<endl;
+
+
+{
+    // string s;
+    // cin>>s;
+    // cout<<s;
+}
+cout<<endl;
+
+
+{
+    // string s;
+    // getline(cin,s);
+    // cout<<s;
+
+}
+cout<<endl;
+
+
+{
+ // count no.of vowels
+    // int n;
+    // cin>>n;
+    // char s[n];
+    // for(int i=0;i<n;i++){
+    //     cin>>s[i];
+    // }
+    // int count=0;
+    // for(int i=0;i<n;i++){
+    //     if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+    //         count++;
+    //     }
+    // }
+    // cout<<"vowels: "<<count;
+}
+cout<<endl;
+
+
+{
+  // count no.of vowels
+    // string s="shekhar suman";
+    // int count=0;
+    // int i=0;
+    // while(s[i]!='\0'){
+    //     if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+    //         count++;
+    //     }
+    //     i++;
+    // }
+    // cout<<"no.of vowels: "<<count;
+}
+cout<<endl;
+
+
+{
+ string str = "shekhar";
+ str[2]='p';
+ cout<<str;
+}
+cout<<endl;
+
+{
+
+}
+}
